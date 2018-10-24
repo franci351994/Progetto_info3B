a05e442b41c9e1e34587d78f7da9cf71aa03d026
Progetto_info3B <f.nannini1@studenti.unibg.it>
2018-10-24 16:54:52 +0200

    primo deploy_01

--- psoccorso/settings.py
+++ psoccorso/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'coda.apps.CodaConfig',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -120,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
